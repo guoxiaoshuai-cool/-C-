f055c125d7b54103a487b4db234f8d4d24c995ce
guoxiaoshuai-cool <1754488715@qq.com>
2021-04-02 23:14:00 +0800

    累了

--- C语言的进阶学习/学习/学习/指针学习.c
+++ C语言的进阶学习/学习/学习/指针学习.c
@@ -304,36 +304,196 @@
 //   return j;
 //}
 
+//
+//	
+//#include<stdio.h>
+//#include<string.h>
+//
+//int main()
+//{
+//	 int i=0;
+//    char a[100];
+//    fgets(a, 80, stdin);
+//   
+//    a[strlen(a)-1]='\0';
+//    for(i=strlen(a)-1; i>=0; i--){
+//         if(a[i]==' '){
+//             printf("%s", &a[i+1]);
+//             printf(" ");
+//             a[i]='\0';
+//         }
+//        if(i==0)
+//            printf("%s", &a[i]);
+//    }
+//    return 0;
 
-	
-#include<stdio.h>
-#include<string.h>
+//求一个数二进制表示数的二进制补码中的一的个数。
 
-int main()
-{
-	 int i=0;
-    char a[100];
-    fgets(a, 80, stdin);
-   
-    a[strlen(a)-1]='\0';
-    for(i=strlen(a)-1; i>=0; i--){
-         if(a[i]==' '){
-             printf("%s", &a[i+1]);
-             printf(" ");
-             a[i]='\0';
-         }
-        if(i==0)
-            printf("%s", &a[i]);
-    }
-    return 0;
-}
+//#include<stdio.h>
+//int count_bit_one(unsigned int n)
+//{
+//	int count = 0;
+//   while(n)
+//   {
+//	   n =n&(n-1);//高效的方法
+//	   count++;
+//   }
+//	//int i = 0;
+//	///*for(i = 0; i<32 ; i++)
+//	//{
+//	//	if(((n >>i)& 1) == 1)
+//	//	{
+//	//		count++;
+//	//	}
+//	//}*/
+//
+//	//while(n)
+//	//{
+//	//	if(n % 2 ==1)
+//	//	{
+//	//		count++;
+//	//	}
+//	//	n = n / 2;
+//	//}
+//	return count;
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d",&a);
+//	int count = count_bit_one(a);
+//	printf("count = %d\n",count);
+//	system("pause");
+//	return 0;
+//}
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int get_diff_bit(int m, int n)
+//{
+//	int count = 0;
+//	int tmp = m^n;
+//	while(tmp)
+//	{
+//		tmp = tmp & (tmp-1);
+//		count++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	int count = 0;
+//	int m = 0;
+//	int n = 0;
+//	scanf("%d%d", &m, &n);
+//	 count = get_diff_bit(m, n);
+//	printf("count = %d\n", count);
+//   return 0;
+//}
+//#include<stdio.h>
+//void print(int n)
+//{
+//	int i = 0;
+//	printf("奇数位：\n");
+//	for(i = 30; i>= 0; i -= 2)
+//	{
+//     printf("%d ", (n>> i) & 1);
+//	}
+//	printf("\n");
+//	printf("偶数位：\n");
+//	for(i = 30; i>= 0; i -= 2)
+//	{
+//     printf("%d ", (n>> i) & 1);
+//	}
+//
+//}
+//int main()
+//{
+//	int m = 0;
+//	scanf("%d", &m);
+//	print(m);
+//	return 0;
+//}
+//void print(int *p, int sz)
+//{
+//	int i = 0;
+//	for(i = 0; i< sz; i++)
+//	{
+//		printf("%d",*(p+i));
+//	}
+//}
+//int mian()
+//{
+//	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(arr ,sz);
+//    return 0;
+//}
 
-
-
-
-
-
-
+//#include<stdio.h>
+//#include<string.h>
+//void reverse_string(char arr[])
+//{
+//	int left = 0;
+//	int right = strlen(arr)-1;
+//	while(left<right)
+//	{
+//	int  tmp = arr[left];
+//	arr[left] = arr[right];
+//	arr[right] = tmp;
+//		left++;
+//	right--;
+//}
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	reverse_string(arr);
+//	printf("%s\n",arr);
+//	return 0;
+//}
+//#include<stdio.h>
+//int DigitSum(unsigned int num)
+//{
+//	if(num > 9)
+//	{
+//		return DigitSum(num / 10) + num %10;
+//	}
+//	else
+//	{
+//		return num;
+//	}
+//}
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%d", &num);
+//	int ret = DigitSum(num);
+//	printf("ret = %d\n",ret);
+//	return 0;
+//}
+//计算N^K
+//#include<stdio.h>
+//double pow(int n, int k)
+//{
+//	if(k <0)
+//		return (1.0/(pow(n,-k)));
+//	else if(k == 0)
+//		return 1;
+//	else
+//		 return n*pow(n, k-1);
+//}
+//
+//int main()
+//{
+//	double ret = 0;
+//	int n = 0;
+//	int k = 0;
+//	scanf("%d%d", &n, &k);
+//	 ret = pow(n, k);
+//	printf("ret = %lf\n",ret);
+//	return 0;
+//
+//}
 
 
 
